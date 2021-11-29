7a664cf87ac718d3e1adb55714759df492cff8f0
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2021-11-29 11:43:58 +0100

    Create cpp program for cpu.

---
+++ src/cpu.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<vector>
+
+#define FILEPATH "/proc/cpuinfo"
+#define DIVIDE 1000.f
+#define MHZ "cpu MHz"
+
+int main (int argc, char ** argv) {
+    std::string line;
+    std::ifstream f(FILEPATH);
+    int max = 0;
+    int average = 0;
+    int freq;
+    int count = 0;
+    while (std::getline(f, line)) {
+        if (! line.rfind(MHZ, 0)) {
+            freq = stoi(line.substr(line.rfind(":") + 1));
+            average += freq;
+            ++count;
+            if (freq > max) max = freq;
+        }
+    }
+    f.close();
+
+    int maxHz = 3600; // (argc > 1) ? (stoi(*(argv + 1))) : 3600;
+    std::string separator = (argc > 2) ? *(argv + 2) : "/";
+
+    average /= count;
+
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << average / DIVIDE << separator;
+    std::cout << max / DIVIDE << separator;
+    std::cout << maxHz / DIVIDE << std::endl;
+
+    return 0;
+}
